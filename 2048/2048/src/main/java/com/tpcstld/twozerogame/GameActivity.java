268bd3cb0ae61344b1f977fff5932ce87ebd9754
Gabriel Firmacion <gabrielfirmacion@gmail.com>
2016-10-26 15:06:51 -0700

    integrate moPub to a game with Skillz

--- 2048/2048/src/main/java/com/tpcstld/twozerogame/GameActivity.java
+++ 2048/2048/src/main/java/com/tpcstld/twozerogame/GameActivity.java
@@ -5,12 +5,19 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.mopub.mobileads.MoPubView;
 import com.skillz.Skillz;
 
-public class GameActivity extends ActionBarActivity {
+public class GameActivity extends AppCompatActivity {
 
     private static final String WIDTH = "width";
     private static final String HEIGHT = "height";
@@ -22,6 +29,7 @@ public class GameActivity extends ActionBarActivity {
     private static final String GAME_STATE = "game state";
     private static final String UNDO_GAME_STATE = "undo game state";
     private MainView view;
+    private MoPubView moPubView;
     public static GameActivity sInstance;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,7 +47,27 @@ public class GameActivity extends ActionBarActivity {
 
         view.hasSaveState = false;
 
-        setContentView(view);
+        // Add Mopub view and MainView to Relative Layout
+        RelativeLayout mainLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams RLParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        mainLayout.setLayoutParams(RLParams);
+        MoPubView adView = new MoPubView(this);
+        adView.setAdUnitId("7a25bb8ad4914b2e99021310caf40e70");
+        adView.loadAd();
+        adView.setId('1');
+
+        RelativeLayout.LayoutParams adViewparams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        adViewparams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+
+        RelativeLayout.LayoutParams gameViewparams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        gameViewparams.addRule(RelativeLayout.BELOW, adView.getId());
+
+        mainLayout.addView(adView, adViewparams);
+        mainLayout.addView(view, gameViewparams);
+
+        setContentView(mainLayout);
     }
 
     @Override
